3c7f84cb39da4d97411d8df35dc25de57714667d
luiznasciment0 <luizp.nascimento@outlook.com>
2021-05-18 00:17:07 -0300

    structuring

---
+++ app/pages/_shared/constants.js
@@ -0,0 +1,12 @@
+export const constants = {
+    socketUrl: 'http://localhost',
+    socketNamespaces: {
+        room: 'room',
+        lobby: 'lobby'
+    },
+    events: {
+        USER_CONNECTED: 'userConnected',
+        USER_DISCONNECTED: 'userDisconnected',
+        JOIN_ROOM: 'joinRoom'
+    }
+}
---
+++ app/pages/room/src/index.js
@@ -0,0 +1,22 @@
+import { constants } from "../../_shared/constants.js"
+import SocketBuilder from "../../_shared/socket.js"
+
+const socketBuilder = new SocketBuilder({
+    socketUrl: constants.socketUrl,
+    namespace: constants.socketNamespaces.room
+})
+
+const socket = socketBuilder
+    .setOnUserConnected((user) => console.log('user connected', user))
+    .setOnUserDisconnected((user) => console.log('user disconnected', user))
+    .build()
+
+const room = {
+    id: Date.now(),
+    topic: 'JS Expert eh noix'
+}
+
+const user = {
+    img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-512.png',
+    username: 'Luiz Nascimento'
+}
